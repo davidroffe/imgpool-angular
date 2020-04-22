8047716ebe9e42cbeac72af075ab957c2823a0b7
David Roffe <davidroffe@gmail.com>
2020-04-21 18:43:27 -0700

    About page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostModule } from './post/post.module';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { PostModule } from './post/post.module';
     BrowserAnimationsModule,
     FormsModule,
     PostModule,
+    AboutModule,
     AppRoutingModule,
   ],
   providers: [],
